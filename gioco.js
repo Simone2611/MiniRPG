fb5c5f26747977138d7295b0ca477b38fe195397
Simone2611 <simone26v@gmail.com>
2025-02-06 14:01:44 +0100

    Creazione file e commenti per iniziare il progetto

---
+++ gioco.js
@@ -0,0 +1,21 @@
+// Prima cosa da fare scegliere nome utente
+// Vita = 100
+// attacco e difesa = valore random
+// Gestire opzioni : attccare curarsi e fuggire
+// Mostro random da un arr di obj
+// fine gioco gestione scoffitta, vittoria o fuga
+// il giocatore può curarsi 3 volte max
+// Fuga in base al mostro
+
+// oltre al gioco uso di Git pubblica con commit con commenti sensati
+
+// voto 1/100
+// Qualità codice
+// Organizzazione
+// qualità della cronologia
+
+// Commit da fare:
+// Creazione Obj Player, Mostri e implementazione Input del Player
+// Funzione per scegliere randomicamente il Mostro (vita, difesa e % di fuga)
+// Funzione per combattere il mostro randomico o fuggire
+// Perdere / vincere
