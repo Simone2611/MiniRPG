dca76fff2a3427c0dccd5f3dd1db9e95fd89676c
Simone2611 <simone26v@gmail.com>
2025-02-06 14:22:48 +0100

    implementazione Input del Player per scegliere il nome + console.log() delle statistche Player + Aggiunta fuga a ogni mostro

--- gioco.js
+++ gioco.js
@@ -14,37 +14,53 @@
 // Organizzazione
 // qualità della cronologia
 
-// Commit da fare:
+// fatto:
 // Creazione Obj Player (nome, vita, attacco random e difesa random), Mostri (vita, attacco random e difesa random)
 // implementazione Input del Player per scegliere il nome + console.log() delle statistche Player
-// Funzione per scegliere randomicamente il Mostro (vita, difesa e % di fuga)
+
+// Commit da fare:
+
+// Funzione per scegliere randomicamente il Mostro dalla lista con la sua vita, difesa e % di fuga
 // Funzione per combattere il mostro randomico o fuggire
 // Perdere / vincere
 
-let player = {
-  nome: input, // nome del player
-  vita: "100", // vita player
-  attacco: Math.floor(Math.random() * 40) + 10, // attacco min 10 max 50
-  difesa: Math.floor(Math.random() * 40) + 10, // difesa min 10 max 50
-};
-
-let mostri = [
-  {
-    nome: "Slime",
-    vita: "30",
-    attacco: Math.floor(Math.random() * 5) + 5, // attacco min 5 max 10
-    difesa: 0, // difesa 0 per lo slime
-  },
-  {
-    nome: "King Slime",
-    vita: "70",
-    attacco: Math.floor(Math.random() * 30) + 10, // attacco min 10 max 40
-    dattacco: Math.floor(Math.random() * 10) + 5, // attacco min 5 max 15
-  },
-  {
-    nome: "GOD Slime",
-    vita: "400",
-    attacco: Math.floor(Math.random() * 50) + 30, // attacco min 30 max 80
-    dattacco: Math.floor(Math.random() * 30) + 10, // attacco min 10 max 30
-  },
-];
+const input = prompt("Inserire un nickname: "); // chiede al player l'input per il nickname
+// se il player cancella non succede niente
+if (input !== null) {
+  function creazionePlayer() {
+    return {
+      nome: input, // nome del player
+      vita: "100", // vita player
+      attacco: Math.floor(Math.random() * 60) + 10, // attacco min 10 max 70
+      difesa: Math.floor(Math.random() * 60) + 10, // difesa min 10 max 70
+    };
+  }
+  let player = creazionePlayer();
+  console.log(`Ciao ${player.nome}`);
+  console.log(`La tua vita: ${player.vita}`);
+  console.log(`Il tuo attacco: ${player.attacco}`);
+  console.log(`Il tuo difesa: ${player.difesa}`);
+  let mostri = [
+    {
+      nome: "Slime",
+      vita: "30",
+      attacco: Math.floor(Math.random() * 5) + 5, // attacco min 5 max 10
+      difesa: 0, // difesa 0 per lo slime
+      fuga: 90,
+    },
+    {
+      nome: "King Slime",
+      vita: "70",
+      attacco: Math.floor(Math.random() * 30) + 10, // attacco min 10 max 40
+      dattacco: Math.floor(Math.random() * 10) + 5, // attacco min 5 max 15
+      fuga: 40,
+    },
+    {
+      nome: "GOD Slime",
+      vita: "400",
+      attacco: Math.floor(Math.random() * 50) + 30, // attacco min 30 max 80
+      dattacco: Math.floor(Math.random() * 30) + 10, // attacco min 10 max 30
+      fuga: 10,
+    },
+  ];
+}
