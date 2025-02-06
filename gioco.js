7e0c345f322344988c1d0bc868cf7691e6e3d661
Simone2611 <simone26v@gmail.com>
2025-02-06 14:16:39 +0100

    Creazione Obj Player (nome, vita, attacco random e difesa random), Mostri (vita, attacco random e difesa random)

--- gioco.js
+++ gioco.js
@@ -15,7 +15,36 @@
 // qualità della cronologia
 
 // Commit da fare:
-// Creazione Obj Player, Mostri e implementazione Input del Player
+// Creazione Obj Player (nome, vita, attacco random e difesa random), Mostri (vita, attacco random e difesa random)
+// implementazione Input del Player per scegliere il nome + console.log() delle statistche Player
 // Funzione per scegliere randomicamente il Mostro (vita, difesa e % di fuga)
 // Funzione per combattere il mostro randomico o fuggire
 // Perdere / vincere
+
+let player = {
+  nome: input, // nome del player
+  vita: "100", // vita player
+  attacco: Math.floor(Math.random() * 40) + 10, // attacco min 10 max 50
+  difesa: Math.floor(Math.random() * 40) + 10, // difesa min 10 max 50
+};
+
+let mostri = [
+  {
+    nome: "Slime",
+    vita: "30",
+    attacco: Math.floor(Math.random() * 5) + 5, // attacco min 5 max 10
+    difesa: 0, // difesa 0 per lo slime
+  },
+  {
+    nome: "King Slime",
+    vita: "70",
+    attacco: Math.floor(Math.random() * 30) + 10, // attacco min 10 max 40
+    dattacco: Math.floor(Math.random() * 10) + 5, // attacco min 5 max 15
+  },
+  {
+    nome: "GOD Slime",
+    vita: "400",
+    attacco: Math.floor(Math.random() * 50) + 30, // attacco min 30 max 80
+    dattacco: Math.floor(Math.random() * 30) + 10, // attacco min 10 max 30
+  },
+];
